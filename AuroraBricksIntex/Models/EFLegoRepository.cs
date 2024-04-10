21c55d5e86d90767b3b45b78e2b9a8d8d3f7fabb
youneedanemail <youneedanemail@gmail.com>
2024-04-10 10:48:31 -0600

    Updated Repo and Controller with CRUD User CRUD is commented out

--- AuroraBricksIntex/Models/EFLegoRepository.cs
+++ AuroraBricksIntex/Models/EFLegoRepository.cs
@@ -11,6 +11,40 @@ namespace AuroraBricksIntex.Models
         }
        public IQueryable<Product> Products => _context.Products;
 
+        public void AddProduct(Product product)
+        {
+            _context.Add(product);
+            _context.SaveChanges();
+        }
 
+        //public void AddUser(User user)
+        //{
+        //    _context.Add(user);
+        //    _context.SaveChanges();
+        //}
+
+        public void DeleteProduct(Product product)
+        {
+            _context.Remove(product);
+            _context.SaveChanges();
+        }
+
+        //public void DeleteUser(User user)
+        //{
+        //    _context.Remove(user);
+        //    _context.SaveChanges();
+        //}
+
+        public void EditProduct(Product product)
+        {
+            _context.Update(product);
+            _context.SaveChanges();
+        }
+
+        //public void EditUser(User user)
+        //{
+        //    _context.Update(user);
+        //    _context.SaveChanges();
+        //}
     }
 }
