bbc20c385b2a3030aa97cc19841222c117e9537c
Tjstevens19 <tdogstrike@gmail.com>
2024-04-10 17:18:59 -0600

    functionality to add products

--- AuroraBricksIntex/Models/Product.cs
+++ AuroraBricksIntex/Models/Product.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AuroraBricksIntex.Models;
 
 public partial class Product
 {
     [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public byte ProductId { get; set; }
 
     public string Name { get; set; } = null!;
