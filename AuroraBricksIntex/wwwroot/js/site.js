ff707a9ab12a60d7b9717e98b53811cbf59199ef
youneedanemail <youneedanemail@gmail.com>
2024-04-11 19:42:24 -0600

    fix description length visually fix link length

--- AuroraBricksIntex/wwwroot/js/site.js
+++ AuroraBricksIntex/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $('.truncated-description').hover(function () {
+        var fullDescription = $(this).data('full-description');
+        $('.description-tooltip .full-description').text(fullDescription);
+        $('.description-tooltip').show();
+    }, function () {
+        $('.description-tooltip').hide();
+    });
+});
